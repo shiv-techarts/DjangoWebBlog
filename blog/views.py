10cc6765884f651742be60c2c81bfb2ca119d877
Shiv Pratap Singh <shiv.safari@gmail.com>
2019-02-06 11:33:13 +0530

    [Day_1 - 0004] Added Dummy Templates and Static CSS and JavaScript Files to test the functionality. Added App in Settings.py File of the main project.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    return render(request=request, template_name='blog/home.html', context={})
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request=request, template_name='blog/about.html', context={})
