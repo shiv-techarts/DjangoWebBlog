adf869fe6e9827a5a1a7084d8cce97de540556d6
Shiv Pratap Singh <shiv.safari@gmail.com>
2019-02-07 00:41:40 +0530

    [Day_2 - 0006] Added html, css, javascript from previous project Modern and Responsive Website and Filled with dummy data to show slider blog content.

---
+++ blog/static/blog/js/base.js
@@ -0,0 +1,110 @@
+/*global $*/
+/*global event*/
+
+// Services Section
+$(function () {
+    // Animate on Scroll
+    new WOW().init();
+});
+
+// Work Section
+$(function () {
+    // Animate on Scroll
+    $('#work').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
+// Team Section
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        loop: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            480: {
+                items: 2,
+            },
+            767: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            }
+        }
+    });
+});
+
+// Clients Section
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 4,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            480: {
+                items: 3,
+            },
+            767: {
+                items: 2,
+            },
+            768: {
+                items: 4,
+            }
+        }
+    });
+});
+
+// Navigation Section
+/*$(function () {
+    
+    $(window).scroll(function() {
+        
+        if($(this).scrollTop() < 50) {
+           $('nav').removeClass("techarts-dark");
+        } else {
+           $('nav').addClass("techarts-dark");
+        }
+        
+    });
+    
+});*/
+
+// Navigation Smooth Scroll
+$(function () {
+
+    $('a.smooth-scroll').click(function () {
+        event.preventDefault();
+
+        var section = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250);
+
+    });
+
+});
+
+// Closing Mobile Menu 
+$(function () {
+
+    $('.navbar-collapse ul li a').on('click touch', function () {
+        $('.navbar-toggler').click();
+    });
+
+});
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,57 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'name': 'Shiv Pratap Singh',
+        'topic': 'Technical',
+        'content': """Technology is the collection of 
+        techniques, skills, methods, and processes used in the production of goods or 
+        services or in the accomplishment of objectives, such as scientific investigation. 
+        Technology can be the knowledge of techniques, processes, and the like, or it can be 
+        embedded in machines to allow for operation without detailed knowledge of their workings. 
+        Systems (e. g. machines) applying technology by taking an input, changing it according 
+        to the system's use, and then producing an outcome are referred to as technology systems 
+        or technological systems.""",
+        'fb_link': "https://fb.com/shiv",
+        'twitter_link': "#",
+        'google_plus_link': "#",
+    },
+    {
+        'name': 'Shiv P Singh',
+        'topic': 'Message',
+        'content': """Technology is the collection of 
+        techniques, skills, methods, and processes used in the production of goods or 
+        services or in the accomplishment of objectives, such as scientific investigation. 
+        Technology can be the knowledge of techniques, processes, and the like, or it can be 
+        embedded in machines to allow for operation without detailed knowledge of their workings. 
+        Systems (e. g. machines) applying technology by taking an input, changing it according 
+        to the system's use, and then producing an outcome are referred to as technology systems 
+        or technological systems.""",
+        'fb_link': "#",
+        'twitter_link': "#",
+        'google_plus_link': "#",
+    },
+    {
+        'name': 'Shiv Singh',
+        'topic': 'Meditation',
+        'content': """Technology is the collection of 
+        techniques, skills, methods, and processes used in the production of goods or 
+        services or in the accomplishment of objectives, such as scientific investigation. 
+        Technology can be the knowledge of techniques, processes, and the like, or it can be 
+        embedded in machines to allow for operation without detailed knowledge of their workings. 
+        Systems (e. g. machines) applying technology by taking an input, changing it according 
+        to the system's use, and then producing an outcome are referred to as technology systems 
+        or technological systems.""",
+        'fb_link': "#",
+        'twitter_link': "#",
+        'google_plus_link': "#",
+    },
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request=request, template_name='blog/home.html', context={})
+    return render(request=request, template_name='blog/home.html', context={'posts': posts})
 
 
 def about(request):
